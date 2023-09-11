824d9c99528be0e3e6153ac8488304108ec9da10
Sean Wadsworth <sw695@sussex.ac.uk>
2023-09-11 14:06:33 +0100

    Added delete function

--- src/main/java/com/seanwads/film_project/controller/FilmController.java
+++ src/main/java/com/seanwads/film_project/controller/FilmController.java
@@ -5,9 +5,9 @@ import com.seanwads.film_project.model.FilmList;
 import com.seanwads.film_project.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path="/demo")
@@ -18,5 +18,23 @@ public class FilmController {
     @GetMapping(path="/all")
     public @ResponseBody Iterable<FilmList> getAllFilms() {
         return filmRepository.findAll();
+    }
+
+    @GetMapping(path="/getID")
+    public @ResponseBody Optional<FilmList> getFilmByID(@RequestParam Integer id){
+        return filmRepository.findById(id);
+    }
+
+    @GetMapping(path="/delete")
+    public @ResponseBody String deleteFilm (@RequestParam Integer id){
+        if(filmRepository.findById(id).isPresent()){
+            FilmList filmToDelete = filmRepository.findById(id).get();
+            String filmName = filmToDelete.getTitle();
+            filmRepository.deleteById(id);
+            return "Film: " + filmName + " deleted";
+        }
+        else {
+            return "Requested film not found";
+        }
     }
 }
