61e398d63914ad87a367869b6afdf57b20cbb2eb
Sean Wadsworth <sw695@sussex.ac.uk>
2023-09-27 16:28:52 +0100

    fixed security hotspot in FilmCategoryController

--- src/main/java/com/seanwads/film_project/controller/FilmCategoryController.java
+++ src/main/java/com/seanwads/film_project/controller/FilmCategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -16,7 +17,7 @@ public class FilmCategoryController {
     @Autowired
     private FilmCategoryRepository filmCategoryRepository;
 
-    @RequestMapping("/getFilmCats")
+    @RequestMapping(path = "/getFilmCats", method = RequestMethod.GET)
     private @ResponseBody Iterable<FilmCategory> getFilmCats(){
         return filmCategoryRepository.findAll();
     }
